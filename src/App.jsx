d9fc0724f8fd86ffcc1ba5213f97d248ae8eea21
KristPS <126523107+KristPS@users.noreply.github.com>
2023-06-27 13:30:53 -0400

    added some codes

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,42 @@
 import {puppyList} from './data.js'
 import { useState } from 'react'
+import './path-to-css.css'
  
 function App() {
+ 
   const [puppies, setPuppies] = useState(puppyList);
   console.log("puppyList: ", puppyList);
+ 
+  const [featPupId, setFeatPupid] = useSate(0);
 
+  function handleClick() {
+  const featuredPup = puppies.find(pup.id === featPupId)
+  console.log("featuredPup: ", featuredPup);
+  }
   return (
-    <>
-      <div className="App">
-     {
-        puppies.map((puppy) => {
-          return <p key={puppy.id}>{puppy.name}</p>;
-        })
-      }
-        
-      </div>
-      
     
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+      <div className="App">
+        <p featPupId={featPupId}></p>
+        {puppies.map((puppy) => {
+          return (
+          <p onClick={handleClick} key={puppy.id}>{puppy.name}
+          </p>
+        );
+     })} 
+    </div>
+  );
 }
 
+ {featPupId && (
+  <div>
+    <h2>{featuredPup.name}</h2>
+    <ul>
+      <li>Age: {featuredPup.age}</li>
+      <li>Email: {featuredPup.email}</li>
+    </ul>
+  </div>
+ )}   
+      
+      
+      
 export default App
